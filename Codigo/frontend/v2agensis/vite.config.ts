6bf5987ee26f50ad71dae7d85523987ec1d25091
Carlos Henrique Neimar <carlos.areas@sga.pucminas.br>
2024-12-26 13:43:25 -0300

    Fix: Docker config finished

--- Codigo/frontend/v2agensis/vite.config.ts
+++ Codigo/frontend/v2agensis/vite.config.ts
@@ -6,14 +6,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    host: true, // Permite acesso externo no Docker
-    port: 5173, // Porta usada pelo Vite
+    host: true,
+    port: 5173,
     watch: {
-      usePolling: true, // Garante que mudanças sejam detectadas no Docker
+      usePolling: true, 
     },
   },
 })
